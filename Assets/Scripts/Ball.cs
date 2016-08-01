cce6d2a52841cddb3ac0c7aadbeb1ca641e3fec7
James Heppell <jheppell100@hotmail.com>
2016-08-01 14:02:20 +0100

    Block breaker Game for web

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	
+	private Paddle paddle;
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+	
+	// Use this for initialization
+	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle>();
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+			// Lock the ball relatie to the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			
+			// Wait for mouse click to begin
+			if (Input.GetMouseButtonDown(0)){
+				Debug.Log("mouse clicked!");
+				hasStarted = true;
+				this.rigidbody2D.velocity = new Vector2 (2f, 10f);
+			}
+		}
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		// Ball does not trigger when brick is destroyed, possibly
+		// due to the brick being destroyed before auido is called?
+		
+		// Careful ball will always speed by, could pick a range with 0 mean?
+		Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f),Random.Range(0f, 0.2f));
+		
+		if (hasStarted) {
+			audio.Play ();
+			rigidbody2D.velocity += tweak;
+		}
+	}
+	
+}
---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	private LevelManager levelmanager;
+
+	void Start() {
+		levelmanager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		Debug.Log ("Collision");
+		
+	}
+	void OnTriggerEnter2D (Collider2D trigger) {
+		Debug.Log ("Trigger");
+		levelmanager.LoadLevel ("Lose");
+	}
+	
+}
